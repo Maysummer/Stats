8237fa630f785ffc5deec63bae7d974a80a8c3b0
Maysummer <udojyk@gmail.com>
2023-11-24 17:54:47 +0100

    analyse data

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,16 @@ const matches = fs
   .map((row: string): string[] => {
     return row.split(',');
   });
-console.log(matches);
+
+//find how many times man united wins
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`ManU won ${manUnitedWins} games`)
