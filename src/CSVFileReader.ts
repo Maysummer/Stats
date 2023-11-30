8b73e112628a98b850236d839431266fa4bf7d71
Maysummer <udojyk@gmail.com>
2023-11-30 18:13:31 +0100

    use an interface based approach

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -1,11 +1,14 @@
 import fs from 'fs';
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
 
-export abstract class CSVFileReader<T> {
-  data: T[] = [];
+//tuple
+type MatchData = [Date, string, string, number, number, MatchResult, string]
+
+export class CSVFileReader {
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
-
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -14,6 +17,16 @@ export abstract class CSVFileReader<T> {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map(this.mapRow);
+      .map((row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult, //type assertion - make sure its one of the MatchResult values
+          row[6],
+        ];
+      });
   }
 }
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,20 +1,8 @@
-import { CSVFileReader } from "./CSVFileReader";
-import { MatchResult } from "./MatchResult";
-import { dateStringToDate } from "./utils";
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
 
-//tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class MatchReader extends CSVFileReader<MatchData> {
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult, //type assertion - make sure its one of the MatchResult values
-      row[6],
-    ];
-  }
+export class MatchReader {
+  constructor(public reader: DataReader){}
 }
