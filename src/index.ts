10d67430c6687d5908dd6ddb92a9fa3795c65d6e
Maysummer <udojyk@gmail.com>
2023-12-04 12:19:34 +0100

    updating reader references

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { CSVFileReader } from './CSVFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CSVFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-console.log(reader.data[0][0])
 //find how many times man united wins
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
