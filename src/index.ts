3c756132cb55e1b53a7e0577cc80730df352df50
Maysummer <udojyk@gmail.com>
2023-12-06 16:44:17 +0100

    generate html reports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { CSVFileReader } from './CSVFileReader';
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HTMLReport } from './reportTargets/HTMLReport';
 
+//Create an object that satisfies the DataReader interface
 const csvFileReader = new CSVFileReader('football.csv');
+
+//Create an instance of MatchReader and pass in something that satisfies the DataReader interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
+const summary = new Summary(new WinsAnalysis('Man United'), new HTMLReport());
 
-
-console.log(`ManU won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
